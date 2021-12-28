629a51aa15b1c35951607c437b5a0312dc2d9e38
yuwenbin <unipower6@hotmail.com>
2021-12-28 22:05:21 +0800

    binary

--- os/binaryshow.c
+++ os/binaryshow.c
@@ -3,24 +3,59 @@
 void show_binary(char c)
 {
 	int i;
+	int array[8];
+	for(i=0;i<8;i++){
+		array[7-i] = (c>>i)&0x01;
+	}
 	printf("[");
 	for(i=0;i<8;i++){
-		printf(" %d",((c<<i)&0x80)>>7);
-		if(i==3)
-			printf("  ");
+		printf("%d",array[i]);
+		if((i+1)%4==0)
+			printf(" ");
 	}
-	printf(" ]");
+	printf("]");
+}
+
+void show_ibin(int i)
+{
+	int j;
+	int array[32];
+	for(j=0;j<32;j++)
+	{
+		array[31-j] = (i>>j)&0x01;
+	}
+
+	printf("[");
+	for(j=0;j<32;j++){
+		printf("%d",array[j]);
+		if((j+1)%4==0)
+			printf(" ");
+	}
+	printf("]");
 }
 
 int main(int argc,char *argv[])
 {
 	char c;
+	int m;
+	unsigned int n;
 
 	printf("Please input char:");
 	scanf("%c",&c);
-	printf("\nhex=[%.2x],binary=",c);
-	show_binary(c);
-
+	for(int i=0;i<8;i++){
+	printf("\nhex=[%.2x],binary=",c>>i);
+	show_binary(c>>i);
 	printf("\n");
+	}
+	printf("Please input integer:");
+	scanf("%d",&m);
+	n = (unsigned int)m;
+	for(int i=0;i<32;i++){
+	printf("\nhex=[%.2x],binary=",n>>i);
+	show_ibin(n>>i);
+	printf("\n");
+	}
+
+
 	return 0;
 }
